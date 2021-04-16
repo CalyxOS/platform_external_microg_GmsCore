993dd0832cd5f54c650fa7114361264e488b6e3e
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-17 00:28:02 +0530

    Spoofing action for the sign in text too
    Change-Id: I9b6f69fdb2594d25155cf6d44bb55f980e3ede91

--- play-services-core/src/main/java/org/microg/gms/auth/login/LoginActivity.java
+++ play-services-core/src/main/java/org/microg/gms/auth/login/LoginActivity.java
@@ -149,7 +149,7 @@ public class LoginActivity extends AssistantActivity {
                 retrieveRtToken(getIntent().getStringExtra(EXTRA_TOKEN));
             }
         } else {
-            setMessage(R.string.auth_before_connect);
+            setMessage(R.string.spoof_auth_before_connect);
             setSpoofButtonText(R.string.brand_spoof_button);
             setBackButtonText(android.R.string.cancel);
             setNextButtonText(R.string.auth_sign_in);
