85753ce0217a22645ac72ebb5b2c1a7bec97dc07
Oliver Scott <olivercscott@gmail.com>
2021-05-10 20:25:14 -0400

    Revert "Import Vanced MicroG brand spoofing login button"
    This reverts commit 784c1a1293d5f48f5ce097baacac6d9192c07c8b.
    
    Reason for revert: Making spoof default
    
    Change-Id: I3c5f16051d4d2d0735c26582114b45233343e495

--- play-services-core/src/main/java/org/microg/gms/checkin/TriggerReceiver.java
+++ play-services-core/src/main/java/org/microg/gms/checkin/TriggerReceiver.java
@@ -35,6 +35,7 @@ import static org.microg.gms.checkin.CheckinService.REGULAR_CHECKIN_INTERVAL;
 
 public class TriggerReceiver extends WakefulBroadcastReceiver {
     private static final String TAG = "GmsCheckinTrigger";
+    private static boolean registered = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
